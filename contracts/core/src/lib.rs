2bb522b3a83529a09a00d60b0f58f65910eea2d3
larry <26318510+larry0x@users.noreply.github.com>
2023-03-22 19:21:42 +0000

    use more memorable reply IDs

--- contracts/core/src/lib.rs
+++ contracts/core/src/lib.rs
@@ -12,6 +12,6 @@ pub const CONTRACT_NAME: &str = "crates.io:one-core";
 pub const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 // reply IDs
-const AFTER_ACTION: u64 = 1;
-const AFTER_ALL_ACTIONS: u64 = 2;
-const AFTER_CALLBACK: u64 = 3;
+const AFTER_ACTION: u64 = 1111;
+const AFTER_ALL_ACTIONS: u64 = 2222;
+const AFTER_CALLBACK: u64 = 3333;
